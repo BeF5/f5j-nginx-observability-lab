3e0f355ce8a35e77719985d96699dfda90f038e9
hiropo20 <hmatsumoto20@gmail.com>
2022-07-08 12:46:05 +0900

    update

--- demotraffic/dummy-access-to-bookinfo.sh
+++ demotraffic/dummy-access-to-bookinfo.sh
@@ -7,6 +7,4 @@ sleep 5
 curl -s -H "Host: bookinfo.example.com" "http://127.0.0.1/product/book" -o /dev/null
 sleep 5
 curl -s -H "Host: bookinfo.example.com" "http://127.0.0.1/" -o /dev/null
-sleep 5
-curl -s -H "Host: bookinfo.example.com" "http://127.0.0.1/" -o /dev/null
 echo "done"
